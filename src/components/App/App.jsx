185ab7672b3e4ce3af257927bcbf6ad404708513
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-04-14 12:51:26 +0100

    wip: initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div id="drum-machine">
+      <div id="display">Hello Drum Machine</div>
+    </div>
+  );
+};
+
+export default App;
